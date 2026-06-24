a6394ea3dafc3c6e512044923704c31b7951a9e1
Kavyan Nayak <kavyannayak104@gmail.com>
2026-06-24 11:39:18 +0530

    user-input

--- input_user.py
+++ input_user.py
@@ -3,5 +3,16 @@
 # a = input("Enter a value: ")
 # print("You entered:", a)
 
-a = input("Enter Your Name : ")
-print("Your Name is :", a)
+# a = input("Enter Your Name : ")
+# print("Your Name is :", a)
+
+
+# only inputs 
+name = input("Enter Your Name : ")
+sport = input("Enter Your Favourite Sport : ")
+education = input("Enter Your Education : ")
+
+# printing the output of the program
+print("Your Name is :", name)
+print("Your Favourite Sport is :", sport)
+print("Your Education is :", education)
