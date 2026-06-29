0e79fcd34a90c07c83d71b8a9a6427653317a324
Kavyan Nayak <kavyannayak104@gmail.com>
2026-06-29 11:44:17 +0530

    assignment operator

---
+++ ansiop.py
@@ -0,0 +1,17 @@
+# assignment operator
+
+# no1 = 100
+# print("The value of no1 is:", no1)
+
+no1 = 100
+no2 = 200
+
+# before swap 
+print("Before swapping: no1 =", no1, "no2 =", no2)
+
+# after swap
+no3 = no1
+no1 = no2
+no2 = no3
+
+print("After swapping: no1 =", no1, "no2 =", no2)
