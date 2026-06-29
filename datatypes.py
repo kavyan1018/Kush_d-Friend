6e0d9d77f31c20e7e94c764284def16694ba88e3
Kavyan Nayak <kavyannayak104@gmail.com>
2026-06-29 11:44:24 +0530

    data types

---
+++ datatypes.py
@@ -0,0 +1,22 @@
+# # float -> decimal values -> 10.20
+
+# no1 = float(input("Enter a decimal value: "))
+# no2 = float(input("Enter a decimal value: "))
+# sum = no1 + no2
+# print("The sum of", no1, "and", no2, "is:", sum)
+
+
+# string -> text values -> "Hello World"
+# name = input("Enter your name: ")
+# print("Hello", name, "! Welcome to the program.")
+
+
+
+# fstring -> formatted string literals
+
+name = "amit"
+age = 23
+salary = 50000
+
+# print("name :", name, "age :", age, "salary :", salary)
+print(f"Name = {name}, Age = {age}, Salary = {salary}")
